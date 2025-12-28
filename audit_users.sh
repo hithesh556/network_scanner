d5eda095df32d0270aa1d777635d1b892436c964
hithesh556 <157955188+hithesh556@users.noreply.github.com>
2025-12-28 17:21:17 +0530

    audit_users.sh

--- audit_users.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "--- Security Audit: Human Users ---"
-echo "Date: $(date)"
-
-# In Linux, human users usually have IDs (UID) greater than 1000.
-# We read /etc/passwd, look for UIDs >= 1000, and print the username.
-awk -F: '$3 >= 1000 {print $1}' /etc/passwd
-
-echo "--- End of Report ---"
